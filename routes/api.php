18634c8aca30ba018ffa3a939bccdde1bc355989
lolo1213 <kejupedas@gmail.com>
2024-01-23 07:47:17 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::withoutmiddleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
